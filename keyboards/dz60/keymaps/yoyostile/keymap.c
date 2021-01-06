663ba363b7636b065fbb7baee477ef7f6244b5bb
Johannes Heck <yoyostile@gmail.com>
2021-01-06 17:00:49 +0100

    even more awesome stuff

--- keyboards/dz60/keymaps/yoyostile/keymap.c
+++ keyboards/dz60/keymaps/yoyostile/keymap.c
@@ -8,13 +8,19 @@
 enum custom_keycodes {
   KC_TRP_GRV,
   KC_BSPC_DEL,
+  KC_OSX_LOCK,
+  KC_BYEBUG,
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case KC_TRP_GRV:
       if (record->event.pressed) {
-        SEND_STRING("```\n\n```" SS_TAP(X_UP));
+        SEND_STRING("```");
+        tap_code16(S(KC_ENTER));
+        tap_code16(S(KC_ENTER));
+        SEND_STRING("```");
+        tap_code(KC_UP);
       }
       break;
     case KC_BSPC_DEL: {
@@ -30,30 +36,40 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       } else {
         unregister_code(kc);
       }
-    }
+      break;
+    };
+    case KC_OSX_LOCK:
+      tap_code16(LCMD(C(KC_Q)));
+      break;
+    case KC_BYEBUG:
+      if (record->event.pressed) {
+        SEND_STRING("require \"byebug\"; byebug");
+      }
+      break;
   }
   return true;
 };
 
 void keyboard_post_init_user(void) {
   rgblight_enable_noeeprom();
-  rgblight_sethsv_noeeprom_teal();
+  rgblight_sethsv_noeeprom(77, 163, 175);
   rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
   switch (get_highest_layer(state)) {
     case _BASE:
-      rgblight_sethsv_noeeprom_teal();
+      // rgblight_sethsv_noeeprom_teal();
+      rgblight_sethsv_noeeprom(77, 163, 175);
       break;
     case _FN:
-      rgblight_sethsv_noeeprom_orange();
+      rgblight_sethsv_noeeprom(77, 163, 175);
       break;
     case _MEDIA:
-      rgblight_sethsv_noeeprom_white();
+      rgblight_sethsv_noeeprom(77, 163, 175);
       break;
     default:
-      rgblight_sethsv_noeeprom_teal();
+      rgblight_sethsv_noeeprom(77, 163, 175);
       break;
   }
   rgblight_mode_noeeprom(1);
@@ -76,13 +92,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*0_*/ KC_GRAVE,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,         KC_F10,        KC_F11,        KC_F12,        KC_INS,    KC_TRP_GRV,
 /*1_*/ RCMD(KC_TAB), RCMD(KC_Q), RCMD(KC_W), RCMD(KC_E), RCMD(KC_R), RCMD(KC_T), RCMD(KC_Y), KC_PGUP,    RCMD(KC_I), RCMD(KC_O),    RCMD(KC_P),    RCMD(KC_LBRC), RCMD(KC_RBRC), KC_DEL,
 /*2_*/ KC_LCTL,      RCMD(KC_A), RCMD(KC_S), KC_PGDN,    RCMD(KC_F), RCMD(KC_G), KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT,      RCMD(KC_SCLN), RCMD(KC_QUOT), RCMD(KC_ENT),
-/*3_*/ KC_TRNS,                  RCMD(KC_Z), RCMD(KC_X), RCMD(KC_C), RCMD(KC_V), RCMD(KC_B), RCMD(KC_N), RCMD(KC_M), RCMD(KC_COMM), RCMD(KC_DOT),  RCMD(KC_SLSH), RCMD(KC_RSFT),            MO(_MEDIA),
+/*3_*/ KC_TRNS,                  RCMD(KC_Z), RCMD(KC_X), RCMD(KC_C), RCMD(KC_V), KC_BYEBUG, RCMD(KC_N), RCMD(KC_M), RCMD(KC_COMM), RCMD(KC_DOT),  RCMD(KC_SLSH), RCMD(KC_RSFT),            MO(_MEDIA),
 /*4_*/ KC_LCTL,        KC_LALT,       KC_LGUI,                                KC_SPC,                                KC_TRNS,       KC_RGUI,       KC_RALT,       KC_RCTL
   ),
 
   [_MEDIA] = LAYOUT_60_ansi_split_bs_rshift(
  //    _0       _1       _2       _3       _4        _5       _6       _7       _8       _9       _A       _B       _C       _D         _E
-/*0_*/ KC_SLEP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   RGB_TOG,
+/*0_*/ KC_OSX_LOCK, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   RGB_TOG,
 /*1_*/ KC_TRNS, KC_BRIGHTNESS_DOWN, KC_BRIGHTNESS_UP, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MAGIC_TOGGLE_NKRO,
 /*2_*/ KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 /*3_*/ KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS,
