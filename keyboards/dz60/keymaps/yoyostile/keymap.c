a852a8b159242ea902ebd21c86b64a9a11409402
Johannes Heck <yoyostile@gmail.com>
2021-01-25 23:58:05 +0100

    add space cadet shift, layer on hold spacebar,...

--- keyboards/dz60/keymaps/yoyostile/keymap.c
+++ keyboards/dz60/keymaps/yoyostile/keymap.c
@@ -51,7 +51,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 };
 
 void keyboard_post_init_user(void) {
-  rgblight_enable_noeeprom();
+  rgblight_disable_noeeprom();
   rgblight_sethsv_noeeprom(77, 163, 175);
   rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
 }
@@ -83,8 +83,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*0_*/ KC_ESC,  KC_1,     KC_2,   KC_3,    KC_4,   KC_5,   KC_6,   KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
 /*1_*/ KC_TAB,  KC_Q,     KC_W,   KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC, KC_RBRC, KC_BSPC_DEL,
 /*2_*/ KC_LCTL, KC_A,     KC_S,   KC_D,    KC_F,   KC_G,   KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT, KC_ENT,
-/*3_*/ KC_LSFT, KC_Z,     KC_X,   KC_C,    KC_V,   KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,           KC_RSFT,          MO(_MEDIA),
-/*4_*/ KC_LCTL, KC_LALT,          KC_LGUI,                 KC_SPC,                            MO(_FN),  KC_RGUI, KC_RALT, KC_RCTL
+/*3_*/ KC_LSPO, KC_Z,     KC_X,   KC_C,    KC_V,   KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,           KC_RSPC,          MO(_MEDIA),
+/*4_*/ KC_LCTL, KC_LALT,          KC_LGUI,                 LT(_FN, KC_SPC),                            MO(_FN),  KC_RGUI, KC_RALT, KC_RCTL
   ),
 
   [_FN] = LAYOUT_60_ansi_split_bs_rshift(
