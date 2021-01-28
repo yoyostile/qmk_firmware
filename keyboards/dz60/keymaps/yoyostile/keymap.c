c79442553c55dce193e0c909c0d70ef6357cdf31
Johannes Heck <yoyostile@gmail.com>
2021-01-28 09:11:16 +0100

    Hi iM kaReN aND I LOVe tO TAlK shIt

--- keyboards/dz60/keymaps/yoyostile/keymap.c
+++ keyboards/dz60/keymaps/yoyostile/keymap.c
@@ -1,4 +1,6 @@
 #include QMK_KEYBOARD_H
+#include <time.h>
+#include <stdlib.h>
 
 // Layers
 #define _BASE   0
@@ -6,11 +8,14 @@
 #define _MEDIA  2
 
 enum custom_keycodes {
-  KC_TRP_GRV,
+  KC_TRP_GRV = SAFE_RANGE,
   KC_BSPC_DEL,
   KC_OSX_LOCK,
   KC_BYEBUG,
+  KC_SHIT_TALK,
 };
+
+static bool shit_talk = false;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
@@ -46,7 +51,47 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         SEND_STRING("require \"byebug\"; byebug");
       }
       break;
+    case KC_SHIT_TALK:
+      if (record->event.pressed) {
+        shit_talk = !shit_talk;
+      }
+      return false;
+    case KC_A:
+    case KC_B:
+    case KC_C:
+    case KC_D:
+    case KC_E:
+    case KC_F:
+    case KC_G:
+    case KC_H:
+    case KC_I:
+    case KC_J:
+    case KC_K:
+    case KC_L:
+    case KC_M:
+    case KC_N:
+    case KC_O:
+    case KC_P:
+    case KC_Q:
+    case KC_R:
+    case KC_S:
+    case KC_T:
+    case KC_U:
+    case KC_V:
+    case KC_W:
+    case KC_X:
+    case KC_Y:
+    case KC_Z: {
+      if (shit_talk) {
+        if (record->event.pressed) {
+          int r = rand();
+          tap_code16((r % 2 == 0) ? S(keycode) : keycode);
+        }
+        return false;
+      }
+    };
   }
+
   return true;
 };
 
@@ -54,6 +99,7 @@ void keyboard_post_init_user(void) {
   rgblight_disable_noeeprom();
   rgblight_sethsv_noeeprom(77, 163, 175);
   rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+  srand(time(NULL));
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
@@ -84,7 +130,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*1_*/ KC_TAB,  KC_Q,     KC_W,   KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC, KC_RBRC, KC_BSPC_DEL,
 /*2_*/ KC_LCTL, KC_A,     KC_S,   KC_D,    KC_F,   KC_G,   KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT, KC_ENT,
 /*3_*/ KC_LSFT, KC_Z,     KC_X,   KC_C,    KC_V,   KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,           KC_RSFT,          MO(_MEDIA),
-/*4_*/ KC_LCTL, KC_LALT,          KC_LGUI,                 LT(_FN, KC_SPC),                            MO(_FN),  KC_RGUI, KC_RALT, KC_RCTL
+/*4_*/ KC_LCTL, KC_LALT,          KC_LGUI,                 KC_SPC,                                      MO(_FN),  KC_RGUI, KC_RALT, KC_RCTL
   ),
 
   [_FN] = LAYOUT_60_ansi_split_bs_rshift(
@@ -93,7 +139,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*1_*/ RCMD(KC_TAB), RCMD(KC_Q), RCMD(KC_W), RCMD(KC_E), RCMD(KC_R), RCMD(KC_T), RCMD(KC_Y), KC_PGUP,    RCMD(KC_I), RCMD(KC_O),    RCMD(KC_P),    RCMD(KC_LBRC), RCMD(KC_RBRC), KC_DEL,
 /*2_*/ KC_LCTL,      RCMD(KC_A), RCMD(KC_S), KC_PGDN,    RCMD(KC_F), RCMD(KC_G), KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT,      RCMD(KC_SCLN), RCMD(KC_QUOT), RCMD(KC_ENT),
 /*3_*/ KC_TRNS,                  RCMD(KC_Z), RCMD(KC_X), RCMD(KC_C), RCMD(KC_V), KC_BYEBUG, RCMD(KC_N), RCMD(KC_M), RCMD(KC_COMM), RCMD(KC_DOT),  RCMD(KC_SLSH), RCMD(KC_RSFT),            MO(_MEDIA),
-/*4_*/ KC_LCTL,        KC_LALT,       KC_LGUI,                                KC_SPC,                                KC_TRNS,       KC_RGUI,       KC_RALT,       KC_RCTL
+/*4_*/ KC_SHIT_TALK,        KC_LALT,       KC_LGUI,                                KC_SPC,                                KC_TRNS,       KC_RGUI,       KC_RALT,       KC_RCTL
   ),
 
   [_MEDIA] = LAYOUT_60_ansi_split_bs_rshift(
